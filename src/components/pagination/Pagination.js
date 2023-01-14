6b2d473462d8a653298ddbd593606bb565b00c01
Fatih Furkan Kocabiyik <82720725+ffurkan10@users.noreply.github.com>
2023-01-14 13:54:31 +0300

    Create pagination component

---
+++ src/components/pagination/Pagination.js
@@ -0,0 +1,42 @@
+import { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchItem, setCurrentPage } from "../../features/ItemSlice";
+import { GrNext, GrPrevious } from "react-icons/gr";
+
+function Pagination() {
+  const dispatch = useDispatch();
+  const currentPage = useSelector((state) => state.data.currentPage);
+  const totalPages = useSelector((state) => state.data.totalPages);
+  const [page, setPage] = useState(currentPage);
+
+  useEffect(() => {
+    setPage(currentPage);
+  }, [currentPage]);
+
+  const handlePageChange = (page) => {
+    setPage(page);
+    dispatch(setCurrentPage(page));
+    dispatch(fetchItem(page));
+  };
+
+  return (
+    <div className="pagination">
+      <button disabled={page === 0} onClick={() => handlePageChange(page - 1)}>
+        <GrPrevious />
+        <span>Previous</span>
+      </button>
+      <span>
+        {page + 1} of {totalPages}
+      </span>
+      <button
+        disabled={page === totalPages - 1}
+        onClick={() => handlePageChange(page + 1)}
+      >
+        <span>Next</span>
+        <GrNext />
+      </button>
+    </div>
+  );
+}
+
+export default Pagination;
