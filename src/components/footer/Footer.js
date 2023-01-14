f888a89768ff3cfb280156cad14a789087dabcfa
Fatih Furkan Kocabiyik <82720725+ffurkan10@users.noreply.github.com>
2023-01-14 13:53:44 +0300

    Create footer component

---
+++ src/components/footer/Footer.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      <div className="footer__container">
+        <div className="footer__container__end">
+          <p>© 2023 Techshop</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
