00e11b67b123e754368a87d7adc96d37e7416e37
Fatih Furkan Kocabiyik <82720725+ffurkan10@users.noreply.github.com>
2023-01-17 13:58:11 +0300

    add hamburger menu

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -4,10 +4,19 @@ import { useSelector } from "react-redux";
 import { useIsLoggedIn } from "../../config/Hooks";
 import { BsHandbag } from "react-icons/bs";
 import logo from "./logo.jpeg";
+import * as FaIcons from "react-icons/fa";
+import * as AiIcons from "react-icons/ai";
+import CgProfile from "react-icons/cg";
+import { useState } from "react";
 
 const Navbar = () => {
+  const [sidebar, setSidebar] = useState(false);
   const cart = useSelector((state) => state.cart);
   const isLoggedIn = useIsLoggedIn();
+
+  const showSidebar = () => {
+    setSidebar(!sidebar);
+  };
 
   return (
     <div className="navbar">
@@ -17,34 +26,50 @@ const Navbar = () => {
             <img src={logo} alt="" />
           </Link>
         </div>
-        <div className="navbar__container__cart">
-          <ul className="navbar__container__cart__ul">
-            <li className="navbar__container__cart__ul__li">
-              <Link to="/">
-                <p>Home</p>
-              </Link>
-            </li>
-            <li className="navbar__container__cart__ul__li">
-              <Link to={isLoggedIn ? "/profile" : "/sign-in"}>
-                <p>{isLoggedIn ? "Your Profile" : "Sign"}</p>
-              </Link>
-            </li>
-            <li className="navbar__container__cart__ul__li">
-              <Link
-                className="navbar__container__cart__ul__li__link"
-                to="/Cart"
-              >
-                <button>
-                  <BsHandbag />
-                </button>
-                <span>
-                  {cart?.cartItems?.length === 0
-                    ? "0"
-                    : cart?.cartItems?.length}
-                </span>
-              </Link>
-            </li>
-          </ul>
+        <div className="navbar__container__menu">
+          <div className="navbar__container__menu__card">
+            <ul
+              className={
+                sidebar
+                  ? `navbar__container__menu__card__ul__active`
+                  : `navbar__container__menu__card__ul`
+              }
+            >
+              <li className="navbar__container__menu__card__ul__li">
+                <Link to="/">
+                  <AiIcons.AiOutlineHome size={"25px"} />
+                  <p>Home</p>
+                </Link>
+              </li>
+              <li className="navbar__container__menu__card__ul__li">
+                <Link to={isLoggedIn ? "/profile" : "/sign-in"}>
+                  <AiIcons.AiOutlineUser size={"25px"} />
+                  <p>{isLoggedIn ? "Your Profile" : "Sign"}</p>
+                </Link>
+              </li>
+              <li className="navbar__container__menu__card__ul__li">
+                <Link to="/Cart">
+                  <button>
+                    <BsHandbag />
+                  </button>
+                  <span>
+                    {cart?.cartItems?.length === 0
+                      ? "0"
+                      : cart?.cartItems?.length}
+                  </span>
+                </Link>
+              </li>
+            </ul>
+          </div>
+          <div className="navbar__container__menu__hamburger">
+            <Link>
+              {sidebar ? (
+                <AiIcons.AiOutlineClose onClick={showSidebar} />
+              ) : (
+                <FaIcons.FaBars onClick={showSidebar} />
+              )}
+            </Link>
+          </div>
         </div>
       </div>
     </div>
